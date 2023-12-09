ee39876b0622a74aa6c5b9f29dd8595384522c67
Gürgün Dayıoğlu <gurgun.dayioglu@icloud.com>
2023-12-09 17:28:16 +0100

    ban + operator for consistency

--- src/index.js
+++ src/index.js
@@ -38,6 +38,7 @@ module.exports = {
           "FunctionDeclaration",
           "VariableDeclaration[kind='var']",
           "UnaryExpression[operator='void']",
+          "UnaryExpression[operator='+']",
           "SequenceExpression",
           "LabeledStatement",
         ],
