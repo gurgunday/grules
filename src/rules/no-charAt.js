cbb9251fa92f17ba0470d5df4b59e6aa5d5c0313
Gürgün Dayıoğlu <hey@gurgun.day>
2024-04-10 18:13:46 +0200

    update noCharAt

--- src/rules/no-charAt.js
+++ src/rules/no-charAt.js
@@ -7,23 +7,18 @@ module.exports = {
       CallExpression: (node) => {
         if (
           node.callee.type === "MemberExpression" &&
-          node.callee.property.name === "charAt"
+          node.callee.property.name === "charAt" &&
+          node.arguments.length === 1
         ) {
-          const objectText = context
-            .getSourceCode()
-            .getText(node.callee.object);
-          const [argument] = node.arguments;
-
-          const replacement =
-            argument?.type === "Literal"
-              ? `${objectText}[${argument.raw}]`
-              : `${objectText}[${context.getSourceCode().getText(argument)}]`;
+          const argument = node.arguments[0];
+          const argumentCode = context.getSourceCode().getText(argument);
+          const objectText = context.sourceCode.getText(node.callee.object);
 
           context.report({
             node,
             message: "Use bracket notation instead of .charAt()",
             fix: (fixer) => {
-              return fixer.replaceText(node, replacement);
+              return fixer.replaceText(node, `${objectText}[${argumentCode}]`);
             },
           });
         }
