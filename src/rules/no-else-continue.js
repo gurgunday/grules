21c351069219a32e28e0712ab43a9d8657989a6c
Gürgün Dayıoğlu <hey@gurgun.day>
2024-05-21 16:29:25 +0200

    better detection

--- src/rules/no-else-continue.js
+++ src/rules/no-else-continue.js
@@ -1,14 +1,19 @@
+const endsWithContinue = (statement) => {
+  if (statement.type === "BlockStatement") {
+    const { body } = statement;
+    return (
+      body.length > 0 && body[body.length - 1].type === "ContinueStatement"
+    );
+  }
+
+  return statement.type === "ContinueStatement";
+};
+
 module.exports = {
   create: (context) => {
     return {
       IfStatement: (node) => {
-        if (
-          node.alternate &&
-          node.consequent.type === "BlockStatement" &&
-          node.consequent.body.some((statement) => {
-            return statement.type === "ContinueStatement";
-          })
-        ) {
+        if (node.alternate && endsWithContinue(node.consequent)) {
           context.report({
             node: node.alternate,
             message: "Unexpected else after continue statement.",
